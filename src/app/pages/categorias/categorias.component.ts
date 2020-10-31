dcaad3b020b2d97c43148a51eae36def7c7f24c1
Jon Lucas <jonatanlucasmolina@gmail.com>
2020-10-31 01:26:19 +0100

    bugs

--- src/app/pages/categorias/categorias.component.ts
+++ src/app/pages/categorias/categorias.component.ts
@@ -18,18 +18,9 @@ export class CategoriasComponent implements OnInit {
      * Cargamos los datos del Json
      */
     this.categoriasService.getData().subscribe(
-      (correct) => {
-        /** Este codigo se realiza para que Angular reconozca que los datos son en JSON. En un caso real, el 
-         * servidor nos devuelve los datos con el un content-type: JSON por lo que ya sabe de que se trata. En este caso
-         * debemos nosotros interpretar esos datos.
-         * 
-         * el código real sería tan solo: var _json = respuestaCorrecta.json() o si ya hemos hecho esta transformación en el service
-         * ya podríamos trabajar con el json sin problema
-         */
-        
-        let _json = JSON.stringify(correct);
+      (correct: any) => {
 
-        this.categorias = JSON.parse(_json).data; 
+        this.categorias = correct.data;
       },
       (error) => {
         console.log("Error getData() | categoriasService | CategoriasComponent ",error);
